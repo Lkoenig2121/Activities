c1ac06d6845a942d179056d1584431bd7210e2a2
Lkoenig2121 <Lkoenig2121@gmail.com>
2022-08-01 13:36:09 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Activities from './components/Activities';
+import './App.css';
+         
+function App() {
+  return (
+      <Activities></Activities> 
+  );     
+}        
+         
+export default App;
